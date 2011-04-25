54a863e27b8e54fb5fb36285becd15c5e5dd86b4
Jacek Pintera <easykey@interia.pl>
2011-04-25 17:15:29 +0200

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Cytat::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
