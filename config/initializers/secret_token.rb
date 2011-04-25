4c88f5dd66b71833b45502f67570de51d2b258fe
Jacek Pintera <easykey@interia.pl>
2011-04-25 15:03:29 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cytat::Application.config.secret_token = '9fffd438bfffe2e7fdf00bb7603450bd98c96dacab9098a0524cc451805c33153684d00add03b58462fa7c6cd058dfd767f52a3b9d1a52dc2512ad732cfa1f6c'
