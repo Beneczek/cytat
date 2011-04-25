8ad33e7a8f8b20b1742f79a805729c5ee62455a8
Jacek Pintera <easykey@interia.pl>
2011-04-26 00:24:22 +0200

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "Rejestracja"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+	flash[:success] = "Witaj! Dziękuję za rejestrację"
+	redirect_to @user
+    else
+      @title = "Rejestracja"
+      render 'new'
   end
 
 end
